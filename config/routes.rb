5be117366e7ec035f4f8a5014161fdbd5dddbc43
MY-PC\Hayakawa Yusuke <yusukeee0412@gmail.com>
2016-09-01 18:52:06 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'prototypes#index'
 
-  get  '/prototypes/index'  =>    'prototypes#index'
-  get  '/prototypes/new'    =>    'prototypes#new'
-  get  '/prototypes/show'   =>    'prototypes#show'
+  resource :prototypes, only: [:index, :new, :show]
+
 end
