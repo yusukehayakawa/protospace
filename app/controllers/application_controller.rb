f94ee63ca6834f4cc335c44e38e81cf58ae7be52
HayakawaYusuke <Yusuke@HayakawaYusuke-no-MacBook-Pro.local>
2016-09-13 16:01:25 +0900

    fix mistake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [
-      :username, :member, :profile, :works, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :member, :profie, :works, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :member, :profile, :works, :avatar])
   end
 
