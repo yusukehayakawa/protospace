6bd9afe7b0ddf360a594be637d8aea77bf32dbcc
HayakawaYusuke <Yusuke@HayakawaYusuke-no-MacBook-Pro.local>
2016-09-23 01:45:54 +0900

    fix prototype table

--- db/migrate/20160917031956_create_prototypes.rb
+++ db/migrate/20160917031956_create_prototypes.rb
@@ -1,7 +1,6 @@
 class CreatePrototypes < ActiveRecord::Migration
   def change
     create_table :prototypes do |t|
-      t.integer :id
       t.string :name
       t.text :catch_copy
       t.text :concept
