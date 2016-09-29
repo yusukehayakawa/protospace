87da24ffd97a9e7cc63d75ea817a22aff37cd295
HayakawaYusuke <Yusuke@HayakawaYusuke-no-MacBook-Pro.local>
2016-09-30 02:26:55 +0900

    add user_id column

---
+++ db/migrate/20160928174120_add_users_ref_to_prototypes.rb
@@ -0,0 +1,5 @@
+class AddUsersRefToPrototypes < ActiveRecord::Migration
+  def change
+    add_reference :prototypes, :user, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917100609) do
+ActiveRecord::Schema.define(version: 20160928174120) do
 
   create_table "images", force: :cascade do |t|
     t.text     "image",        limit: 65535
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 20160917100609) do
     t.text     "concept",    limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id",    limit: 4
   end
+
+  add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
@@ -52,4 +55,5 @@ ActiveRecord::Schema.define(version: 20160917100609) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "prototypes", "users"
 end
