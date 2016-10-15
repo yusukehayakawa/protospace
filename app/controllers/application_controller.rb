48e0c4550470188edc6e61f24c9b387042bf044c
HayakawaYusuke <Yusuke@HayakawaYusuke-no-MacBook-Pro.local>
2016-10-15 16:02:00 +0900

    fix controller bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  add_flash_types :success, :warning
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :member, :profie, :works, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :member, :profile, :works, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :member, :profile, :works, :avatar])
   end
 
