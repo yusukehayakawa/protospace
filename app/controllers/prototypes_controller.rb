e5aa1a761b4ecf4c3da4f2f08698131eb0c2acc6
HayakawaYusuke <Yusuke@HayakawaYusuke-no-MacBook-Pro.local>
2016-10-17 18:06:03 +0900

    add destroy action and delete link

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -30,12 +30,18 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    
     @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
        redirect_to :root, success: "Prototype was successfully updated."
     else
       render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
+      redirect_to :root, success: "Prototype was successfully deleted."
     end
   end
 
