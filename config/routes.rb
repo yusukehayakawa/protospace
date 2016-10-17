adfa158839478235bd9eb68cf0618cdd22d73ba4
HayakawaYusuke <Yusuke@HayakawaYusuke-no-MacBook-Pro.local>
2016-10-17 18:04:48 +0900

    add destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
+
   resources :users, only: [:show, :edit]
-  resources :prototypes, only: [:new, :show, :create, :edit, :update]
+  resources :prototypes, only: [:new, :show, :create, :edit, :update, :destroy]
 
 end
