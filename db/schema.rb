0c9442ea7082263c115af8f514e6e64286b37c58
HayakawaYusuke <Yusuke@HayakawaYusuke-no-MacBook-Pro.local>
2016-10-18 11:43:59 +0900

    add likes_count column to prototypes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928174120) do
+ActiveRecord::Schema.define(version: 20161018023027) do
 
   create_table "images", force: :cascade do |t|
     t.text     "image",        limit: 65535
@@ -21,13 +21,21 @@ ActiveRecord::Schema.define(version: 20160928174120) do
     t.datetime "updated_at"
   end
 
-  create_table "prototypes", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.text     "catch_copy", limit: 65535
-    t.text     "concept",    limit: 65535
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id",    limit: 4
+  end
+
+  create_table "prototypes", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "catch_copy",  limit: 65535
+    t.text     "concept",     limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id",     limit: 4
+    t.integer  "likes_count", limit: 4
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
