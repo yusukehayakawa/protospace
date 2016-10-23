2a34c6a22a912dccd8520e234a6088745950157f
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-10-24 02:56:03 +0900

    fix db

--- db/migrate/20161023174736_addproto_to_prots.rb
+++ db/migrate/20161023174736_addproto_to_prots.rb
@@ -1,6 +1,6 @@
 class AddprotoToProts < ActiveRecord::Migration
   def change
-    add_column :prototypes, :likes_count, :integer, :default => 0
-    add_column :prototypes, :comments_count, :integer, :default => 0
+    add_column :prototypes, :likes_count, :integer, default: 0
+    add_column :prototypes, :comments_count, :integer, default: 0
   end
 end
