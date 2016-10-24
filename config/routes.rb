d72a74a146a1dda4f1d2c378c83bcde8be91b4bf
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-10-24 22:34:01 +0900

    add comments function to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit]
   resources :prototypes do
     resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create]
   end
 end
