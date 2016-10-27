640549b14ea73425003b9b01d42d370a85526f2b
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-10-27 23:34:38 +0900

    remove unneeded code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
 
-  def index
-    @user = User.find(params[:id])
-  end
-
   def show
     @user = User.find(params[:id])
     @prototypes = @user.prototypes
