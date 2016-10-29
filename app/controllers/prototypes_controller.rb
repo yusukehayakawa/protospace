393ef7959dbafabbf786952a9c1c219c2b992275
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-10-30 03:26:22 +0900

    fix prototypes controller to add popular function

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,7 +3,7 @@ class PrototypesController < ApplicationController
   before_action :find_params, only: [:show, :edit, :destroy]
 
   def index
-     @prototypes = Prototype.includes(:user)
+     @prototypes = Prototype.includes(:user).order('id DESC')
   end
 
   def show
