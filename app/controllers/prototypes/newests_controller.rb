a5006dba5d4e2b66ac96d81b7d6d10c16e6c02a0
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-11-01 12:55:52 +0900

    create newest index file and add newest function

--- app/controllers/prototypes/newests_controller.rb
+++ app/controllers/prototypes/newests_controller.rb
@@ -2,6 +2,5 @@ class Prototypes::NewestsController < ApplicationController
 
   def index
      @prototypes = Prototype.includes(:user).order('id DESC')
-     render template: "prototypes/index"
   end
 end
