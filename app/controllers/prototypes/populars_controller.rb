e16e5032fc35af8736e8121b4ef27ee11d6a6aa7
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-11-01 20:42:22 +0900

    create popular index file

--- app/controllers/prototypes/populars_controller.rb
+++ app/controllers/prototypes/populars_controller.rb
@@ -1,6 +1,5 @@
 class Prototypes::PopularsController < ApplicationController
   def index
      @prototypes = Prototype.includes(:user).order('likes_count DESC')
-     render template: "prototypes/index"
   end
 end
