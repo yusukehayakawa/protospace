33562fbdf7a144c843dd9d089865fb94fc6c5a72
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-11-05 12:18:23 +0900

    fix n+1 problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @prototypes = @user.prototypes
+    @prototypes = @user.prototypes.includes(:tags)
   end
 end
