3c1cbe37413c4f3911ce7cd55633cdee7661aabd
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-11-06 22:49:51 +0900

    add pagination function to view file of users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @prototypes = @user.prototypes.includes(:tags)
+    @prototypes = @user.prototypes.includes(:tags).page(params[:page]).per(8)
   end
 end
