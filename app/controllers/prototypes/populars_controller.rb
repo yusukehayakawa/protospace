b72d41d8a767e47c2c03b834186d677761118856
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-11-06 20:39:25 +0900

    add pagination function to controller file

--- app/controllers/prototypes/populars_controller.rb
+++ app/controllers/prototypes/populars_controller.rb
@@ -1,5 +1,5 @@
 class Prototypes::PopularsController < ApplicationController
   def index
-     @prototypes = Prototype.includes(:user, :tags).order('likes_count DESC')
+     @prototypes = Prototype.includes(:user, :tags).order('likes_count DESC').page(params[:page]).per(8)
   end
 end
