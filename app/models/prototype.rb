aebdca52e174c1093357219e470db3fc42558423
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-11-26 22:02:47 +0900

    fix validation

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,9 +4,9 @@ class Prototype < ActiveRecord::Base
   has_many :images, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  validates_associated :images
+  validates_associated :images, presence: true
   validates :name, :catch_copy, :concept, presence: true
-  accepts_nested_attributes_for :images, reject_if: proc { |attributes| attributes[:image].blank? }
+  accepts_nested_attributes_for :images, limit: 4, reject_if: proc { |attributes| attributes[:image].blank? }
   acts_as_taggable
 
   def like_user(user_id)
