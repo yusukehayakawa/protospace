efc0fcf6ae89c20092e909d025362eac7202d1a2
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-12-01 01:21:05 +0900

    fix factories file

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,6 +1,6 @@
 include ActionDispatch::TestProcess
 FactoryGirl.define do
   factory :image do
-    image    Faker::Avatar.image
+    image  { fixture_file_upload("spec/fixtures/aaproto4-2.jpg", 'image/png') }
   end
 end
