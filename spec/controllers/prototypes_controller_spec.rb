5692f876644ee3797cc07ab713b12ec8d8ed8246
HayakawaYusuke <yusukeeee0412@gmail.com>
2016-12-18 18:34:36 +0900

    prototype controller test

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -1,214 +1,220 @@
 require 'rails_helper'
 
 describe PrototypesController, type: :controller do
-  let(:prototype){create(:prototype)}
 
-  # describe 'GET #new' do
-  #   it "assigns the requested prototypes to @prototype" do
-  #       get :new
-  #       expect(assigns(:prototype)).to be_a_new(Prototype)
-  #   end
+  describe 'GET #new' do
+    before do
+      login_user
+    end
+    it "assigns the requested prototypes to @prototype" do
+        get :new
+        expect(assigns(:prototype)).to be_a_new(Prototype)
+    end
 
-  #   it "renders the :new template" do
-  #       get :new, id: 1
-  #       expect(response).to render_template :new
-  #   end
-  # end
+    it "renders the :new template" do
+        get :new
+        expect(response).to render_template :new
+    end
+  end
 
   describe 'POST #create' do
     context 'with valid attriubtes' do
-        user = login_user
-        let(:prototype){create(:prototype)}
+      before do
+        login_user
+        @images = [attributes_for(:image, status: "main"),
+                   attributes_for(:image, status: "sub")]
+      end
+      let(:prototype){ create(:prototype) }
       it "saves the new prototype in the database" do
-        binding.pry
-        expect{post :create, prototype: prototype.id, prototype: attributes_for(:prototype)}.to change(Prototype, :count).by(1)
+        expect{ post :create, prototype: prototype.id, prototype: attributes_for(:prototype) }.to change(Prototype, :count).by(1)
       end
 
-      # it "redirects to root_path" do
-      #   expect(response).to redirect_to new_user_session_path
-      # end
+      it "redirects to root_path" do
+        post :create, prototype: prototype.id, prototype: attributes_for(:prototype, images_attributes: @images)
+        expect(response).to redirect_to root_path
+      end
 
-      # it "shows flash messages to show save the prototype successfully" do
-      #   expect(flash[:success]).to eq "Successfully created your prototype."
-      # end
+      it "shows flash messages to show save the prototype successfully" do
+        post :create, prototype: prototype.id, prototype: attributes_for(:prototype, images_attributes: @images)
+        expect(flash[:success]).to eq "Successfully created your prototype."
+      end
     end
 
     context 'with invalid attriubtes' do
+      before do
+        login_user
+      end
+      let(:prototype){ build(:prototype) }
       it "dose not save the new prototype in the database" do
         post :create, prototype: attributes_for(:prototype)
         expect{post :create, prototype: attributes_for(:prototype)}.not_to change(Prototype, :count)
       end
 
-      it "redirects to new_prototype_path" do
+      it "render template new_prototype_path" do
         post :create, prototype: attributes_for(:prototype)
-        expect(response).to render_template new_prototype_path(assigns[:prototype])
-      end
-
-      it "shows flash messages to show save the prototype unsuccessfully" do
-        expect(flash[:alert]).to be_present
+        expect(response).to render_template :new
       end
     end
   end
 
-  # describe 'GET #show' do
-  #   it "assigns the requested prototype to @prototype" do
-  #     prototype = create(:prototype)
-  #     get :show, id: prototype
-  #     expect(assigns(:prototype)).to eq prototype
-  #   end
+  describe 'GET #show' do
+    let(:prototype){ create(:prototype) }
+    it "assigns the requested prototype to @prototype" do
+      get :show, id: prototype
+      expect(assigns(:prototype)).to eq prototype
+    end
 
-  #   it "assigns the requested comment to @comment" do
-  #     prototype = create(:prototype)
-  #     comment = create(:comment, prototype: prototype)
-  #     get :show, id: prototype
-  #     expect(assigns(:prototype)).to eq prototype
-  #   end
+    it "assigns the requested comment to @comment" do
+      comment = create(:comment, prototype: prototype)
+      get :show, id: prototype
+      expect(assigns(:prototype)).to eq prototype
+    end
 
-  #   it "assigns likes associated with prototype to @likes" do
-  #     prototype = create(:prototype)
-  #     like = create(:like, prototype: prototype)
-  #     get :show, id: prototype
-  #     expect(assigns(:prototype)).to eq prototype
-  #   end
+    it "assigns likes associated with prototype to @likes" do
+      like = create(:like, prototype: prototype)
+      get :show, id: prototype
+      expect(assigns(:prototype)).to eq prototype
+    end
 
-  #   it "renders the :show template" do
-  #     prototype = create(:prototype)
-  #     get :show, id: prototype
-  #     expect(response).to render_template :show
-  #   end
-  # end
+    it "renders the :show template" do
+      get :show, id: prototype
+      expect(response).to render_template :show
+    end
+  end
 
-  # describe 'GET #edit' do
-  #   it "assigns the requested prototype to @prototype" do
-  #     prototype = create(:prototype)
-  #     get :edit, id: prototype
-  #     expect(assigns(:prototype)).to eq prototype
-  #   end
+  describe 'GET #edit' do
+    let(:prototype){ create(:prototype) }
+    before do
+      login_user
+    end
+    it "assigns the requested prototype to @prototype" do
+      get :edit, id: prototype
+      expect(assigns(:prototype)).to eq prototype
+    end
 
-  #   it "assigns the main_image to @main_image" do
-  #     prototype = create(:prototype)
-  #     image =build(:image, status: 0, prototype: prototype)
-  #     image2 =build(:image, status: 1, prototype: prototype)
-  #     get :edit, id: prototype
-  #     expect(assigns(:prototype)).to eq prototype
-  #   end
+    it "assigns the main_image to @main_image" do
+      image =build(:image, status: 0, prototype: prototype)
+      image2 =build(:image, status: 1, prototype: prototype)
+      get :edit, id: prototype
+      expect(assigns(:prototype)).to eq prototype
+    end
 
-  #   it "assigns the sub_images to @sub_images" do
-  #     prototype = create(:prototype)
-  #     image =build(:image, status: 0, prototype: prototype)
-  #     image2 =build(:image, status: 1, prototype: prototype)
-  #     get :edit, id: prototype
-  #     expect(assigns(:prototype)).to eq prototype
-  #   end
+    it "assigns the sub_images to @sub_images" do
+      image =build(:image, status: 0, prototype: prototype)
+      image2 =build(:image, status: 1, prototype: prototype)
+      get :edit, id: prototype
+      expect(assigns(:prototype)).to eq prototype
+    end
 
-  #   it "renders the :edit template" do
-  #     prototype = create(:prototype)
-  #     get :edit, id: prototype
-  #     expect(response).to render_template :edit
-  #   end
-  # end
+    it "renders the :edit template" do
+      get :edit, id: prototype
+      expect(response).to render_template :edit
+    end
+  end
 
-  # describe 'PATCH #update' do
-  #   context 'with valid attriubtes' do
-  #     it "assigns the requested prototype to @prototype" do
-        
-        
-  #     end
+  describe 'PATCH #update' do
+    before do
+        login_user
+        @prototype = create(:prototype, name: "yusuke")
+      end
+    context 'with valid attriubtes' do
+      it "assigns the requested prototype to @prototype" do
+        patch :update, id: @prototype, prototype: attributes_for(:prototype)
+        expect(assigns(:prototype)).to eq(@prototype)
+      end
 
-  #     it "assigns the requested comment to @comment" do
-        
-        
-  #     end
+      it "assigns the requested comment to @comment" do
+        comment = create(:comment, prototype: @prototype)
+        patch :update, id: @prototype.id, prototype: attributes_for(:prototype)
+        expect(@prototype.comments).to include comment
+      end
 
-  #     it "updates attriubtes of prototype" do
-        
-        
-  #     end
+      it "updates attriubtes of prototype" do
+        patch :update, id: @prototype, prototype: attributes_for(:prototype, name: "hayakawa")
+        @prototype.reload
+        expect(@prototype.name).to eq("hayakawa")
+      end
 
-  #     it "redirects to prototype_path" do
-        
-        
-  #     end
+      it "redirects to root_path" do
+        patch :update, id: @prototype, prototype: attributes_for(:prototype)
+        expect(response).to redirect_to root_path
+      end
 
-  #     it "shows flash messages to show updates prototype successfully" do
-        
-        
-  #     end
-  #   end
+      it "shows flash messages to show updates prototype successfully" do
+        patch :update, id: @prototype, prototype: attributes_for(:prototype)
+        expect(flash[:success]).to eq "Prototype was successfully updated."
+      end
+    end
 
-  #   context 'with invalid attriubtes' do
-  #     it "assigns the requested prototype to @prototype" do
-        
-        
-  #     end
+    context 'with invalid attriubtes' do
+      it "dose not save the new prototype" do
+        patch :update, id: @prototype, prototype: attributes_for(:prototype, name: "hayakawa")
+        expect{ @prototype.reload }.to change(Prototype, :count).by(0)
+      end
 
-  #     it "dose not save the new prototype" do
-        
-        
-  #     end
+      it "renders the :edit template" do
+        patch :update, id: @prototype, prototype: attributes_for(:prototype, name: "")
+        expect(response).to render_template :edit
+      end
+    end
+  end
 
-  #     it "renders the :show template" do
-        
-        
-  #     end
+  describe 'DELETE #destroy' do
+    before do
+      login_user
+      @prototype = create(:prototype)
+    end
 
-  #     it "shows flash messages to show updates prototype unsuccessfully" do
-        
-        
-  #     end
-  #   end
-  # end
+    it "deletes the prototype" do
+      expect{delete :destroy, id: @prototype}.to change(Prototype, :count).by(-1)
+    end
 
-  # describe 'DELETE #destroy' do
-  #   it "assigns the requested prototypes to @prototype" do
-  #     delete :destory, id: prototype
-  #     expect(assigns(:prototype)).to eq prototype
-  #   end
+    it "redirects to root_path" do
+        delete :destroy, id: @prototype
+        expect(response).to redirect_to root_path
+    end
 
-  #   it "deletes the prototype" do
-  #       expect{delete :destroy, id: @prototype}.to change(Prototype, :count).by(-1)
-  #   end
-  #   it "redirects to root_path" do
-  #       delete :destroy, id: @prototype
-  #       expect(response).to redirect_to root_path
-  #   end
+    it "shows flash messages to show delete prototype successfully" do
+        delete :destroy, id: @prototype
+        expect(flash[:success]).to eq "Prototype was successfully deleted."
+    end
+  end
 
-  #   it "shows flash messages to show delete prototype successfully" do
-  #       delete :destroy, id: @prototype
-  #       expect(prototype.flash[:success]).to include("successfully")
-  #   end
-  # end
+  describe 'without user login' do
+    let(:prototype){ create(:prototype) }
+    context 'GET #new' do
+      it "redirects sign_in page" do
+        get :new
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
 
-  # describe 'without user login' do
-  #   context 'GET #new' do
-  #     it "redirects sign_in page" do
-  #       get :new
-  #       expect(response).to redirect_to new_user_session_path
-  #     end
-  #   end
+    context 'POST #create' do
+      it "redirects sign_in page" do
+        post :create
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
 
-  #   context 'POST #create' do
-  #     it "redirects sign_in page" do
-  #       expect(response).to redirect_to new_user_session_path
-  #     end
-  #   end
+    context 'GET #edit' do
+      it "redirects sign_in page" do
+        get :edit, id: prototype
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
 
-  #   context 'GET #edit' do
-  #     it "redirects sign_in page" do
-  #       expect(response).to redirect_to new_user_session_path
-  #     end
-  #   end
+    context 'PATCH #update' do
+      it "redirects sign_in page" do
+        patch :update, id: prototype
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
 
-  #   context 'PATCH #update' do
-  #     it "redirects sign_in page" do
-  #       expect(response).to redirect_to new_user_session_path
-  #     end
-  #   end
-
-  #   context 'DELETE #destroy' do
-  #     it "redirects sign_in page" do
-  #       expect(response).to redirect_to new_user_session_path
-  #     end
-  #   end
-  # end
+    context 'DELETE #destroy' do
+      it "redirects sign_in page" do
+        delete :destroy, id: prototype
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
 end
